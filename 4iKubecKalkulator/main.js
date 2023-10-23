97c4fbc4a9223573a805ff0a186af18f791a2a76
JKUBEC <102363976+bubenciak@users.noreply.github.com>
2023-10-23 10:27:46 +0200

    Add files via upload

---
+++ 4iKubecKalkulator/main.js
@@ -0,0 +1,12 @@
+const display = document.querySelector(".input");
+const buttons = document.querySelectorAll("button");
+const operations = ["%", "*", "/", "-", "+", "="];
+
+let output = "";
+
+const calculate = (btnValue) => { tu funkcja ale nie umiem
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => calculate(event.target.value));
+});
